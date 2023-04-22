a83f4fbbcf961f76a7d88a95d4c0f9b6c2fac243
JonathanCouck <78811390+JonathanCouck@users.noreply.github.com>
2023-04-22 17:23:37 +0200

    Add basic styling

---
+++ src/global/styles/colors.ts
@@ -0,0 +1,34 @@
+export default {
+  primary: '',
+  primaryDark: '',
+  primaryDarker: '',
+  primaryLight: '',
+  primaryLighter: '',
+
+  secondary: '',
+  secondaryDark: '',
+  secondaryDarker: '',
+  secondaryLight: '',
+  secondaryLighter: '',
+
+  primaryText: '',
+  primaryTextDark: '',
+  primaryTextDarker: '',
+  primaryTextLight: '',
+  primaryTextLighter: '',
+
+  secondaryText: '',
+  secondaryTextDark: '',
+  secondaryTextDarker: '',
+  secondaryTextLight: '',
+  secondaryTextLighter: '',
+
+  background: '',
+  backgroundDark: '',
+  backgroundDarker: '',
+  backgroundLight: '',
+  backgroundLighter: '',
+
+  white: '',
+  black: '',
+}
---
+++ src/global/styles/screenSizes.ts
@@ -0,0 +1,6 @@
+export default {
+  small: '0rem',
+  medium: '48rem',
+  large: '64rem',
+  extraLarge: '120rem',
+}
